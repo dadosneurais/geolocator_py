626f56f21b52f8ae58e72343b3b647ede4d92234
dadosneurais <140826720+dadosneurais@users.noreply.github.com>
2024-08-05 05:17:10 +0000

    Add files via upload

---
+++ geolocator.py
@@ -0,0 +1,16 @@
+import requests
+
+def public_ip():
+    return requests.get('https://api.ipify.org').text # ip público
+
+def get_location(ip):
+    url = f'http://ipinfo.io/{ip}/json'
+    response = requests.get(url)
+    data = response.json()
+    return data['loc']  # latitude e longitude
+
+location = get_location(public_ip())
+google_maps_url = f'https://www.google.com/maps?q={location}'
+
+print(f'Meu IP público é: {public_ip()}')
+print(f'URL do Google Maps: {google_maps_url}')
